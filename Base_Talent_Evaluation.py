91532ac8851c566c03b5af134d084ceada215cd7
AmperSign <33861417+AmperSign@users.noreply.github.com>
2017-11-21 01:48:30 -0800

    Update Base_Talent_Evaluation.py

--- Base_Talent_Evaluation.py
+++ Base_Talent_Evaluation.py
@@ -1,1 +1,29 @@
+#Lmao hey so whats up AMpersigning it just wanted to say hey signing in and get to work :) 11/21//2017 
+#1:33Am AmperSign Enjoy 
+
+
+Skills_Talents List Of General Directory
+#The order I Want to do them in
+
+
+Martial_Arts
+Working_Out
+KenesieAlogy
+Neuroscience
+Enginering
+Economics
+Statistics
+algabra
+applied Algabra
+calculis
+applied calculis
+geomotry
+Applied Geomortry
+applied behavoiral psychology
+
+
+Mathamatics
+Neuroscience
+Enginering
+Martial_Arts
 
